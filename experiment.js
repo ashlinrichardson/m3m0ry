dcec438ebe474dcbf841c5bf341285847232981d
Ashlin Richardson <arichardson@kinsolresearch.com>
2017-01-23 01:30:25 -0800

    blocking out some methods to fill in...
    blocking out some methods to fill in...

--- experiment.js
+++ experiment.js
@@ -7,6 +7,29 @@ function instructions(txt, ctx){
   return x;
 };
 
+/* formerly known as orientation task*/
+function study_phase(ctx){
+};
+
+/* formerly known as recognition task */
+function test_phase(ctx){
+};
+
+/* formerly known as feedback task */
+function response_task(ctx){ 
+  // there might be a mode where this functionality has to mix with test-phase. 
+
+  // block these out by trial first... 
+};
+
+function delay_task(ctx){
+  // list as many countries as possible during a 3-minute period.
+  // play a video. 
+};
+
+function stimuli_pool(ctx){
+};
+
 function test(){
   console.log('test []');
   var x = new state(ctx);
