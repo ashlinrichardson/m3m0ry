38e01928c5ba0c62bfe962c8b591b1895c8205a2
Ashlin Richardson <arichardson@kinsolresearch.com>
2017-01-23 01:32:55 -0800

    tidy up
    tidy up

--- key_board.js
+++ key_board.js
@@ -6,27 +6,20 @@ function unicode_from_key_event(e){
 /* keyboard status array (unicode format).. */
 var key_unicode = {};
 
-function keyboard_module() {
+function keyboard_module(){
 
-  /* key down is sufficient.. Delete elsewhere.. */
+  /* key down sufficient.. delete record elsewhere.. */
   document.onkeydown = function(e) {
     var unicode = unicode_from_key_event(e);
     var key =  String.fromCharCode(unicode);
     key_unicode[unicode] = true;
-
+    /* start a new life */ 
     var now = ctx.get_state();
-    console.log('(that was now = ctx.get_State()');
     if(now && now.key_expiry){
         ctx.clear_tmr();
         now.expire();
     }
-    /*
-    if(onUpdate){
-      onUpdate(key_unicode);
-    }*/
   }
   return key_unicode;
-
 }
-
 
