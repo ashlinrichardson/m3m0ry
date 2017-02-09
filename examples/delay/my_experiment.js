00b9db41d80abe67eacf42bbdb9d8ade0586fa91
Ash <arichardson@kinsolresearch.com>
2017-02-09 00:16:47 -0800

    test
    test

--- examples/delay/my_experiment.js
+++ examples/delay/my_experiment.js
@@ -5,13 +5,11 @@ var my_experiment = function(){
 
   instructions('delay phase: please press any key to start')
 
-  delay_task('please type in the names of as many countries as you can think of in 5 seconds, separated by spaces...press any key to begin', 
-  
-    5000)
+  delay_task('please type in the names of as many countries as you can think of in 5 seconds, separated by spaces...press any key to begin'
+    ,5000)
 
-  delay_task('please type in the names of as many countries as you can think of in 30 seconds, separated by spaces...press any key to begin', 
-  
-    30000)
+  delay_task('please type in the names of as many countries as you can think of in 30 seconds, separated by spaces...press any key to begin'
+    ,30000)
 
 instructions('all done.. thank you')
   
