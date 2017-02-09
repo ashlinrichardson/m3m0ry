665cb7782286601f15486546b97568fb3748c5ac
Ash <arichardson@kinsolresearch.com>
2017-02-09 00:16:03 -0800

    Update examples
    Update examples

--- examples/feedback/my_experiment.js
+++ examples/feedback/my_experiment.js
@@ -5,14 +5,15 @@ var my_experiment = function(){
   // some instructions before `test phase'
   instructions('feedback coming up...')
     
-  feedback('please enter your affinity with the last stimulus on a scale of 1-5', [49, 50, 51, 52, 53])
+  feedback('please enter your affinity with the last stimulus on a scale of 1-5', 
+    [49, 50, 51, 52, 53])
 
-instructions('thank you... more feedback coming up...')
+  instructions('thank you... more feedback coming up...')
 
-  feedback('please enter your affinity with the last stimulus on a scale of 0-9', [49, 50, 51, 52, 53, 54, 55, 56, 57, 48])
+  feedback('please enter your affinity with the last stimulus on a scale of 0-9', 
+    [49, 50, 51, 52, 53, 54, 55, 56, 57, 48])
 
-instructions('thank you')
-
+  instructions('thank you')
 
 
 }
