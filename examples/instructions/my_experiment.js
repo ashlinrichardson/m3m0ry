ba198624d78cea989dba72d9136ab96745c55a70
Ash <arichardson@kinsolresearch.com>
2017-03-01 11:55:29 -0800

    From demo
    From demo at UVIC wednesday two weeks ago….

--- examples/instructions/my_experiment.js
+++ examples/instructions/my_experiment.js
@@ -2,7 +2,7 @@
 
 var my_experiment = function(){
   //set up some instruction slides..
-  instructions('welcome to the recognition memory experiment framework')
+  instructions('welcome to the recognition memory experiment framework. here is what happens when you put in a lot of text. if you put in a lot of text then it might go over the edge.')
   instructions('this is an instructions slide')
   instructions('this is another instructions slide') 
   
