8dd871de011ac9edc4a84d5431ee386d52086031
Ash <arichardson@kinsolresearch.com>
2017-04-17 23:55:27 -0700

    test
    test

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
-import cgi, cgitb, surveyWebEnvironment
-cgitb.enable(); form = cgi.FieldStorage()
-message = "hello world"   
-surveyWebEnvironment.printHTMLmsg(message)
+import os
+import cgi
+import cgitb
+import datetime
+import surveyWebEnvironment; printf = surveyWebEnvironment.printHTMLmsg
+cgitb.enable()
+form = cgi.FieldStorage()
+
+printf("str(form)="+str(form))
+printf("data=" + str(form.getvalue('data')))
+printf(str(datetime.datetime.now().isoformat()))
+
+
+pwd = os.getcwd() + '/'
+
+dat_f = pwd + '/data/'
+
+if not os.path.exists(dat_f):
+  os.mkdir(dat_f)
+
+printf(pwd)
