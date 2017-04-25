d643831c2d4d93b0dcc3b6b6e3981b7e8ac60c06
Ash <arichardson@kinsolresearch.com>
2017-04-25 13:36:42 -0700

    test
    test

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
+'''
+  this script is for receiving the data and saving it to a file. 
+'''
 import os
 import cgi
 import cgitb
 import datetime
-import surveyWebEnvironment; printf = surveyWebEnvironment.printHTMLmsg
+import surveyWebEnvironment
+
 cgitb.enable()
 form = cgi.FieldStorage()
+printf = surveyWebEnvironment.printHTMLmsg
 
-printf("str(form)="+str(form))
-printf("data=" + str(form.getvalue('data')))
-printf(str(datetime.datetime.now().isoformat()))
-
+#printf("str(form)="+str(form))
+#printf("data=" + str(form.getvalue('data')))
+date_str = str(datetime.datetime.now().isoformat())
+printf(date_str)
 
 pwd = os.getcwd() + '/'
-
-dat_f = pwd + '/data/'
-
+dat_f = pwd + 'data/'
 if not os.path.exists(dat_f):
   os.mkdir(dat_f)
 
-printf(pwd)
+dat_fn = dat_f + date_str + ".txt"
+
+printf(dat_fn)
+
+f = open(dat_fn, "wb")
+#f.write("str(form)="+str(form)+"\n");
+f.write(str(form.getvalue('data'))+"\n")
+f.close()
+# printf(pwd)
