1cbf78dbd9a0ace401d87e58382805c75d8a4e33
Ash <arichardson@kinsolresearch.com>
2017-04-25 13:45:29 -0700

    test complete (tidy up)
    test complete (tidy up)

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python
-'''
-  this script is for receiving the data and saving it to a file. 
-'''
+'''this script is for receiving the data and saving it to a file. 
+  xml_send.html (in the example) sends the data. this script recieves it.'''
 import os
 import cgi
 import cgitb
 import datetime
-import surveyWebEnvironment; printf = surveyWebEnvironment.printHTMLmsg
 cgitb.enable()
 form = cgi.FieldStorage()
 
-#printf("str(form)="+str(form))
-printf("data=" + str(form.getvalue('data')))
 date_str = str(datetime.datetime.now().isoformat())
-#printf(date_str)
 
 pwd = os.getcwd() + '/'
 dat_f = pwd + 'data/'
@@ -22,10 +17,6 @@ if not os.path.exists(dat_f):
 
 dat_fn = dat_f + date_str + ".txt"
 
-#printf(dat_fn)
-
 f = open(dat_fn, "wb")
-#f.write("str(form)="+str(form)+"\n");
 f.write(str(form.getvalue('data'))+"\n")
 f.close()
-# printf(pwd)
