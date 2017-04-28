8868b46aa92f48c6eb2dedda01f29096c8cffbd9
Ash <arichardson@kinsolresearch.com>
2017-04-27 20:05:01 -0700

    Pep8 and simplify python code
    Pep8 and simplify python code

--- xml_receive.py
+++ xml_receive.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python
-'''this script is for receiving the data and saving it to a file. 
-  xml_send.html (in the example) sends the data. this script recieves it.'''
+''' this server-side python-CGI script receives text format data sent
+over the internet by the client-side function util.js::xml_send()'''
 import os
 import cgi
-import cgitb
 import datetime
-cgitb.enable()
-form = cgi.FieldStorage()
-
-date_str = str(datetime.datetime.now().isoformat())
 
 pwd = os.getcwd() + '/'
+
 dat_f = pwd + 'data/'
 if not os.path.exists(dat_f):
     os.mkdir(dat_f)
 
-dat_fn = dat_f + date_str + ".txt"
+dat_fn = dat_f + str(datetime.datetime.now().isoformat()) + ".txt"
 
+# retrieve CGI form data
 dat_str = None
 try:
-    dat_str = str(form.getvalue('data'))+"\n"
+    dat_str = str(cgi.FieldStorage().getvalue('data')) + '\n'
 except:
     pass
 
+# write the data to file in the data/ folder
 if dat_str:
-    f = open(dat_fn, "wb")
-    f.write(dat_str)
-    f.close()
+    f = open(dat_fn, 'wb').write(dat_str)
