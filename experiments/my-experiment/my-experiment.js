508e975a5983b1f3f683cc176482a6d8e5e95a61
Ash <arichardson@kinsolresearch.com>
2017-05-11 21:46:33 -0700

    add "user" experiment
    add "user" experiment

---
+++ experiments/my-experiment/my-experiment.js
@@ -0,0 +1,35 @@
+/* recognition memory experiment set-up */
+
+var my_experiment = function(){
+
+  /* set up some instruction slides */
+  instructions('study phase: please remember images and press any key')
+
+  /* set up a stimulus pool */
+  var p = pool()
+
+  /* add images to stimulus pool */
+  for(var i = 0; i < 10; i++){
+    p.add(ctx.imgs[i])
+  }
+
+  /* add words to stimulus pool */ 
+  p.add('floccinaucinihilipilification')
+  p.add('supercalifragilisticexpialidocious')
+  p.add('umdiddlediddlediddleumdiddlei')
+
+  /* selection from stimulus pool (parameters are N, M) */
+  p.select(3, 3)
+
+  /* set up `study phase': show selected portions of pool */
+  study_phase(p, 111)
+
+  /* some instructions before `test phase' */
+  instructions('test phase coming up')
+  instructions('when you see an image/word, please press m or n')
+  instructions('please press m if you saw an image/word before')
+  instructions('please press n if you did not see the image/word before')
+ 
+  /* set up `test phase' (user input recorded for whole randomized pool) */
+  test_phase(p, 333)
+}
