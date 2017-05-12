4bae8d45a1398d2a549542bdb044e0d6388bf20b
Ash <arichardson@kinsolresearch.com>
2017-05-11 17:06:16 -0700

    Next: allow array of pools / or single pool
    study_phase, test_phase constructor needs to take a single pool, or an
    array of pools…

--- task.js
+++ task.js
@@ -21,10 +21,7 @@ function instructions(txt){
 /* study phase, formerly known as orientation task: multiple `trials' / events occur here.. random selection of inputs... (for the test phase, the random selection is shuffled back into the pool).. */
 function study_phase(my_pool, isi=0){
 
-/* this section isn't right yet.. need an M,N parameter, and an array of pools! (multiple pools..) 
-
-    SAME FOR TEST PHASE SECTION...
-  */
+/* need ARRAY OF POOLS.... also need to specify parameters N,M */
   var trial_index = -1
   var my_task_id = next_task_id++
 
