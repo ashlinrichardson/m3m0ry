c0cc456c08912bc8ac81486b89b8b439091c309e
ashlinrichardson <richardson.ashlin@gmail.com>
2017-05-23 21:30:01 -0700

    tidy up
    tidy up

--- experiments/delay/my-experiment.js
+++ experiments/delay/my-experiment.js
@@ -1,9 +1,9 @@
 /* recognition memory experiment set-up */
 var my_experiment = function(){
 
-  instructions('first delay phase (please press <esc> key to end): (please press any key to continue)')
+  instructions('first delay phase coming up (please press any key to continue)')
 
-  delay_task('please write out anything that comes to mind (please press <esc> key when finished) (please press any key to continue)')
+  delay_task('please write out anything that comes to mind (please press <esc> key when finished)')
 
   /* instruction slide */
   instructions('second delay phase (5 seconds): (please press any key to continue)')
@@ -18,9 +18,9 @@ var my_experiment = function(){
   x.set_expiry(5000)
   x.key_expiry = false
 
-  instructions('third delay phase (10 seconds): (please press any key to continue)')
+  instructions('third delay phase (6 seconds): (please press any key to continue)')
 
-  /* set up delay task: 10 seconds */
+  /* set up delay task: 6 seconds */
   delay_task('please type names of as many countries as you can think of in 6 seconds, separated by spaces.. (please press any key to continue)',
              6000 /* 10000 mS */)
 
