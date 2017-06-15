48b330f8d373d18014eda5caf367657b4a3d2a30
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-14 19:46:21 -0700

    cgi/python test file
    cgi/python test file

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print 'Content-Type: text/html;charset=utf-8\n'
+print '<html><h1>h3ll0 w0r1d</h1></html>'
