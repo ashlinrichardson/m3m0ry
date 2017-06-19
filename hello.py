d447f5c59adba67ce78a09715cb671953d4b50cd
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-06-19 09:32:14 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+'''20170619 
+this file for testing if python/CGI is enabled'''
 
 # enable debugging
 import cgitb
 cgitb.enable()
 
+# hello world message (HTML)
 print 'Content-Type: text/html;charset=utf-8\n'
 print '<html><h1>h3ll0 w0r1d</h1></html>'
